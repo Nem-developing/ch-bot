52adffb1e226043206d6205bf1ad01bda1e922f0
Nem-developing <54665744+Nem-developing@users.noreply.github.com>
2020-03-21 15:17:03 +0100

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
     client.user.setPresence({
         game: {
-            name: "!ch help | V3.20.33 | Made by Nem#2318" // Texte affiché dans la liste des membres du serveur et sur le profile du bot.
+            name: "!ch help | V1.5.2 | Made by Nem#2318" // Texte affiché dans la liste des membres du serveur et sur le profile du bot.
         }
     });
 };
