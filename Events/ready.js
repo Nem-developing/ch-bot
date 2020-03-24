cef0785fd2d8e1548affea598c72e0f8d800e1b3
Nem-developing <54665744+Nem-developing@users.noreply.github.com>
2020-03-24 19:49:49 +0100

    Add files via upload

--- Events/ready.js
+++ Events/ready.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
     client.user.setPresence({
         game: {
-            name: "!ch help | V2.0.1 | Made by Nem#2318" // Texte affiché dans la liste des membres du serveur et sur le profile du bot.
+            name: "!ch help | V3.20.55 | Made by Nem#2318"
         }
     });
 };
