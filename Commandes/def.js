25dfe3549f6a1a490b295db46fac297d144598e1
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-04-06 02:47:22 +0200

    Ajout d'une chaîne de test.
    Prise en compte d'erreur et résolutions de celles-ci vis à vis de la nature de la réponse des dictionnaires.

--- Commandes/def.js
+++ Commandes/def.js
@@ -7,22 +7,46 @@ module.exports.run = (client, message, args) => {
         return message.channel.send("Veuillez spécifier un mot à définir !\n\n Utilisation : `!ch def [mot_à_deffinir]`"); }
 
 
-	var req = unirest("GET", `https://dicolink.p.rapidapi.com/mot/${args[0]}/definitions?limite=3`);
+	var req = unirest("GET", `https://dicolink.p.rapidapi.com/mot/${args[0]}/definitions?limite=4`);
 
 	req.headers({
 		"x-rapidapi-host": "dicolink.p.rapidapi.com",
         "x-rapidapi-key": dico.key
 	});
 
-	req.end(function (res) {
-		try {
-			console.log(res.body)
-			console.log("def 1")
-			console.log(res.body[0].definition)
-			console.log("def 2")
-			console.log(res.body[1].definition)
-			console.log("def 3")
-			console.log(res.body[2].definition)
+    req.end(function (res) {
+        try {
+            console.log(res.body)                   // affichage de la réponsse complette de l'API
+            var nature = "";                        // initialisation de la variable nature (type string)
+
+
+            if (!res.body[0].nature) {              // Suite à un petit bug où le premier dictionnaire ne trouvait pas la nature, j'ai créé ce procédé pour éviter la présence d'un blanc dans la case "Nature".                                                   
+
+                nature = "NONE"
+            } else {
+                nature = res.body[0].nature
+            }
+            if (!res.body[1].nature) {
+
+                nature = "NONE"
+            } else {
+                nature = res.body[1].nature
+            }
+            if (!res.body[2].nature) {
+
+                nature = "NONE"
+            } else {
+                nature = res.body[2].nature
+            }
+            if (!res.body[3].nature) {
+
+                nature = "NONE"
+            } else {
+                nature = res.body[3].nature
+            }
+            if (nature === "NONE") {
+                nature = "Nature indéfinie"
+            }
 
             message.channel.send({
                 embed: {
@@ -33,20 +57,28 @@ module.exports.run = (client, message, args) => {
                     title: `Deffinition de __${args[0]}__ :`,
                     description: `Voici les differentes déffinitions du mot ${args[0]} :`,
                     fields: [{
+                        name: "Source :",
+                        value: `**${ res.body[0].source }**`,
+                    },
+                    {
                             name: "Nature :",
-                            value: `__${res.body[0].nature}__`
+                            value: `__${ nature }__`,
                     },
                     {
-                        name: `**${res.body[0].source}** :`,
-                        value: `${res.body[0].definition}`
+                        name: `** Signification 1** :`,
+                        value: `${ res.body[0].definition }`,
                     },
                     {
-                        name: `**${res.body[1].source}** :`,
+                        name: `** Signification 2** :`,
                         value: `${res.body[1].definition}`
                     },
                     {
-                        name: `**${res.body[2].source}** :`,
+                        name: `** Signification 3** :`,
                         value: `${res.body[2].definition}`
+                    },
+                    {
+                            name: `** Signification 4** :`,
+                            value: `${res.body[3].definition}`
                     }
                     ],
                     timestamp: new Date(),
@@ -59,7 +91,7 @@ module.exports.run = (client, message, args) => {
 
 		} catch (error) {
 			console.error(res.error);
-			message.channel.send(`Le mot est inconue du dico.`)
+			message.channel.send(`Le mot est inconue du dictionnaire dsl...`)
 		}
 
 
