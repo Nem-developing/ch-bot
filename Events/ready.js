899917979e9f261a1818bf9f3ca9d6edf391943c
Nem <54665744+Nem-developing@users.noreply.github.com>
2020-04-14 12:06:55 +0200

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,3 +1,3 @@
 module.exports = (client) => {
-    client.user.setActivity('!ch help | V4.20.72 | Made by Nem#2318', { type: "PLAYING"});
+    client.user.setActivity('!ch help | V4.20.122 | Made by Nem#2318', { type: "PLAYING"});
 };
