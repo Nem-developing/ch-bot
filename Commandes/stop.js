f88f3c6f6ff749364ec36fbfb3d75dd9f0feb88a
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-08-15 16:34:47 +0200

    Nouvel outil d'administration.

---
+++ Commandes/stop.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js'); // Import de la bibliothéque "discord.js".
+
+module.exports.run = (client, message, args) => {
+	if (!message.guild.member(message.author).hasPermission('MANAGE_MESSAGES')) {
+		return;
+	}
+
+
+    // Premièrement on doit récuperer l'id du channel actuel.
+    let channel = message.channel;
+    let roles = message.guild.roles; // collection
+    const aderants = message.guild.roles.cache.find(r => r.name === 'Adhérent');
+
+
+
+    // On retire la vue pour les nouveaux du salon "Ouverture-Fermeture".
+    channel.updateOverwrite(aderants, { SEND_MESSAGES: false });
+
+
+    message.channel.send(`======================================================`)
+    message.channel.send(`**Un membre du staff vient de figer ce salon, veuillez patienter.**`)
+    message.channel.send(`======================================================`)
+
+
+
+
+};
+
+module.exports.help = {
+	name: 'stop'
+};
+
+
