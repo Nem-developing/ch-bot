ca30eac7621d449eb2612cfce4ab3eef0958397f
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-08-15 16:33:21 +0200

    Nouvel outil d'administration.

---
+++ Commandes/start.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js'); // Import de la bibliothéque "discord.js".
+
+module.exports.run = (client, message, args) => {
+    if (!message.guild.member(message.author).hasPermission('MANAGE_MESSAGES')) {
+        return;
+    }
+
+
+    // Premièrement on doit récuperer l'id du channel actuel.
+    let channel = message.channel;
+    let roles = message.guild.roles; // collection
+    const aderants = message.guild.roles.cache.find(r => r.name === 'Adhérent');
+
+
+    // On retire la vue pour les nouveaux du salon "Ouverture-Fermeture".
+    channel.updateOverwrite(aderants, { SEND_MESSAGES: true });
+
+
+    message.channel.send(`__**Tchat ré-ouvert !**__`)
+
+
+
+};
+
+module.exports.help = {
+    name: 'start'
+};
+
+
