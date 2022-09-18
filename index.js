67617f05774c8d2c7568f1b80cff3dea2614b72d
Nem-Developing <nem05yt@gmail.com>
2022-09-18 15:53:04 +0200

    :bug:

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
             GatewayIntentBits.GuildMessages,
             GatewayIntentBits.MessageContent,
             GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildMessageReactions,
         ],
     });
 
