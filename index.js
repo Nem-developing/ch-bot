c6bd117844508197a9c27110d067bb7eac4f687a
Nem-Developing <nem05yt@gmail.com>
2022-09-18 15:33:37 +0200

    :bug:

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
     const Discord = require('discord.js');
-    const path = require('node:path');
     const { Client, GatewayIntentBits } = require('discord.js')
-    const client = new Client({
-        intents: [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.MessageContent,
-            GatewayIntentBits.GuildMembers,
-        ]
-    })
+ 
+    const myIntents = new IntentsBitField();
+    myIntents.add(IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildMembers);
+    
+    const client = new Client({ intents: myIntents });
 
     const token = require("./jsons/token.json");  
     const fs = require('node:fs');  
