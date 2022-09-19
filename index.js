b2cf9f7261600bf970a3f90afc1ff17058fe8cdd
Nem-developing <nem05yt@gmail.com>
2022-09-19 23:10:40 +0200

    Try to fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
     const { Client, GatewayIntentBits, Partials } = require('discord.js');
 
     const client = new Client({
-        intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMembers],
+        intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMembers, GatewayIntentBits.DirectMessages],
         partials: [Partials.Message, Partials.Channel, Partials.Reaction],
     });
 
