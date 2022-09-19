e79d06cd312176baa42eebae57b730c3a948bc88
Nem-developing <nem05yt@gmail.com>
2022-09-19 22:45:54 +0200

    Try to fix

--- Commandes/stats.js
+++ Commandes/stats.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 module.exports.run = (client, message, args) => {
     const membre = message.mentions.members.first() || message.member;
     if (!membre) { return message.channel.send('Veuillez mentionner un utilisateur !'); }
-    message.channel.send(`Statistiques de l'utilisateur **${membre.user.username}** \n\n --> ID : **${membre.id}** \n --> Compte créé le **${moment.utc(membre.user.createdAt).format("LL")}** \n --> Présent sur Chrétiens-Fr depuis le **${moment.utc(membre.joinedAt).format('LL')}**`)
+    message.channel.send(`Statistiques de l'utilisateur **${membre.user.username}** \n\n --> ID : **${membre.id}** \n --> Compte créé le **${moment.utc(membre.user.createdAt).format("Do MMMM YYYY")}** \n --> Présent sur Chrétiens-Fr depuis le **${moment.utc(membre.joinedAt).format('Do MMMM YYYY')}**`)
 };
 
 module.exports.help = {
