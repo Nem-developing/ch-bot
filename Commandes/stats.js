9b25255e05bdf55fa7a890d042aafeaa1e6f4aa3
Nem-developing <nem05yt@gmail.com>
2022-09-19 22:30:52 +0200

    Try to fix

--- Commandes/stats.js
+++ Commandes/stats.js
@@ -1,41 +1,26 @@
 const Discord = require('discord.js'); // Import de la bibliothèque "discord.js".
 const moment = require('moment');
-
+const { EmbedBuilder } = require('discord.js');
 
 module.exports.run = (client, message, args) => {
     const membre = message.mentions.members.first() || message.member;
     if (!membre) { return message.channel.send('Veuillez mentionner un utilisateur !'); }
     message.channel.send(membre.user.presence)
-    message.channel.send({
-        embed: {
-            color: 3447003,
-            title: `Statistiques de l'utilisateur **${membre.user.username}**`,
-            thumbnail: {
-                url: membre.user.displayAvatarURL
-            },
-            fields: [
-                {
-                    name: 'ID :',
-                    value: membre.id 
-                },
-                {
-                    name: 'Crée le :',
-                    value: moment.utc(membre.user.createdAt).format("LL")
-                },
-                {
-                    name: 'Jeu :',
-                    value: membre.user.presence ? membre.user.presence : 'Aucun jeu'
-                },
-                {
-                    name: 'Rejoin le :',
-                    value: moment.utc(membre.joinedAt).format('LL')
-                }
-            ],
-            footer: {
-                text: `Informations de l'utilisateur ${membre.user.username}`
-            }
-        }
-    });
+
+
+
+    const exampleEmbed = new EmbedBuilder()
+        .setColor(3447003)
+        .setTitle(`Statistiques de l'utilisateur **${membre.user.username}**`)
+        .addFields(
+            { name: 'ID :', value: membre.id },
+            { name: 'Crée le :', value: moment.utc(membre.user.createdAt).format("LL") },
+            { name: 'Rejoin le :', value: membre.id },
+        )
+        .setTimestamp()
+        .setFooter({ text: `Informations de l'utilisateur ${membre.user.username}`});
+
+    channel.send({ embeds: [exampleEmbed] });
 };
 
 module.exports.help = {
