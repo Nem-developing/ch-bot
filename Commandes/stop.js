6fc2a9e8abcab9f55c96582dabf7b3eba099df2c
Nem-developing <nem05yt@gmail.com>
2022-09-19 21:45:52 +0200

    Try to fix

--- Commandes/stop.js
+++ Commandes/stop.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'); // Import de la bibliothéque "discord.js".
 
 module.exports.run = (client, message, args) => {
-	if (!message.member.hasPermission("BAN_MEMBERS")) {
+	if (!message.member.permissions.has("MANAGE_MESSAGES")) {
 		return;
 	}
 
