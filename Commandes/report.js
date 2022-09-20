bdca7b05ba604564bf817861723ddcf619b47b80
Nem-developing <nem05yt@gmail.com>
2022-09-20 23:25:12 +0200

    Try to fix

--- Commandes/report.js
+++ Commandes/report.js
@@ -20,12 +20,9 @@ module.exports.run = (client, message, args) => {
 
    
     // Envoie du message au rapporteur
-    message.author.send(`Veuillez spécifier le message à rapporter au staff concernant le membre : ${args[0]} (Exemple : Ce membre m'a insulté).\n\n\n**__(Faites attentions, Le staff pourait vous banir si il décrète que vous mentez)__**`)
-    const msg_filter = (m) => m.author.id === message.author.id;
-    message.author.dmChannel.awaitMessages({ filter: msg_filter, max: 1 })
-    .then((collected) => {
-        console.log(collected)
-    });
+    let channeldm = message.author.dmChannel;
+    message.channeldm.send(`Veuillez spécifier le message à rapporter au staff concernant le membre : ${args[0]} (Exemple : Ce membre m'a insulté).\n\n\n**__(Faites attentions, Le staff pourait vous banir si il décrète que vous mentez)__**`)
+    
 
 
 };
