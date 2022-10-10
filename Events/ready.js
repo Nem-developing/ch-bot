c6ddeb3273aa7e1b61d7e08fe7ecaf7e467ac39e
Nem-developing <nem05yt@gmail.com>
2022-10-10 10:49:41 +0200

    :poop: Test

--- Events/ready.js
+++ Events/ready.js
@@ -1,10 +1,5 @@
 module.exports = (client) => {
-    client.user.setStatus('online')
-    client.user.setPresence({
-        game: {
-            name: '!ch help | V22.9 | Made by Nem#2318',
-            type: "Playing",
-            url: ""
-        }
-    });
+    console.log("test")
+    client.user.setActivity(`on ${client.guilds.cache.size} servers`);
+
 };
