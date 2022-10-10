5de5fe0d05659d45b37478a30b4d88e84df6d708
Nem-developing <nem05yt@gmail.com>
2022-10-10 10:43:05 +0200

    :poop: Test

--- Events/ready.js
+++ Events/ready.js
@@ -1,3 +1,11 @@
 module.exports = (client) => {
-    client.user.setActivity('!ch help | V22.9 | Made by Nem#2318', { type: "PLAYING"});
+    client.user.setActivity('', { type: "PLAYING"});
+    client.user.setStatus('online')
+    client.user.setPresence({
+        game: {
+            name: '!ch help | V22.9 | Made by Nem#2318',
+            type: "Playing",
+            url: ""
+        }
+    });
 };
